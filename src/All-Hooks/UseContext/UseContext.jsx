731e7a0753118fc3e6f50fcd9de6d2ca194032c4
Rumon-Sarker <mdsrrumon1234@gmail.com>
2024-09-04 16:07:27 +0600

    All React Hooks

--- src/All-Hooks/UseContext/UseContext.jsx
+++ src/All-Hooks/UseContext/UseContext.jsx
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 
+//  context api 
 
 export const AuthProvider = createContext();
 
--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ function App() {
       {/* <UseContext /> exchange to <UserProfile/> */}
       <UserProfile />
       <UseReducer />
-
     </>
   )
 }
