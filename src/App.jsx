27ea62ba49c41b7eac4791f818be8a09a9fecf21
Rumon-Sarker <mdsrrumon1234@gmail.com>
2024-09-04 16:05:41 +0600

    all-custom-hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import CustomHooks from './All-Hooks/CustomHooks/CustomHooks'
 import UseCallback from './All-Hooks/UseCallback/UseCallback'
-import UseContext from './All-Hooks/UseContext/UseContext'
 import UserProfile from './All-Hooks/UseContext/UserProfile'
 import UseEffect from './All-Hooks/UseEffect/UseEffect'
-import UseLayoutEffect from './All-Hooks/UseLayoutEffect/UseLayoutEffect'
 import UseMemo from './All-Hooks/UseMemo/UseMemo'
 import UseReducer from './All-Hooks/UseReducer/UseReducer'
 import UseRef from './All-Hooks/UseRef/UseRef'
@@ -22,8 +19,7 @@ function App() {
       {/* <UseContext /> exchange to <UserProfile/> */}
       <UserProfile />
       <UseReducer />
-      {/* <UseLayoutEffect /> */}
-      {/* <CustomHooks /> */}
+
     </>
   )
 }
